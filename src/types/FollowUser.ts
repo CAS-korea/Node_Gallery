884d6073528c47fe5635504d89f689b5f9a8403a
BAE GEUNWOO <92969924+GWB21@users.noreply.github.com>
2025-02-28 22:55:55 +0900

    CREATE : 팔로워, 팔로잉 받아오는 dto 생성

---
+++ src/types/FollowUser.ts
@@ -0,0 +1,6 @@
+export interface FollowUser {
+    userId: string;         // 사용자 ID
+    name: string;           // 사용자 이름
+    role: string;           // 사용자 역할 (예: STUDENT, PROFESSOR 등)
+    profileImageUrl: string; // 프로필 이미지 URL
+}
